0b42f78230bef79d857b32ed75845815c0d1aeed
sayandip paul <sayandip.web@gmail.com>
2026-03-13 20:45:58 +0530

    Function in python

---
+++ ch-07.py
@@ -0,0 +1,21 @@
+#function in python (defination)
+
+def hello():
+    print("hellow world")
+
+hello()
+hello()
+hello()
+hello()
+hello()
+
+
+def add():
+    a=10
+    b=20
+    print(a+b)
+add()
+add()
+
+
+
